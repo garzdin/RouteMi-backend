9335141fdae00b3d2e5d9046b2e161281b25f27c
Teodor Garzdin <tsg@hugelawn.com>
2016-04-05 10:22:58 +0200

    Add JWT and middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_URL || 'mongodb://apiuser:4pv-aeh-PbH-Btw@ds013300.mlab.com:13300/routemiapi');
 var Kitten = require('./models/kitten.js');
@@ -16,8 +17,35 @@ app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
+app.set('tokenKey', process.env.TOKENKEY || 'Q353oF8Dp4NX51XJwdG7sIaI43l4JXyeRDClR0TYR5aPKBcUleRkyyprgQBR79U');
+
 app.get('/', function(request, response) {
   response.send({ response: "Welcome to the API." })
+});
+
+app.get('/authenticate', function(request, response) {
+  var token = jwt.sign('RouteMiApiJWT', app.get('tokenKey'));
+  response.send({ token: token });
+});
+
+app.use(function(request, response, next) {
+  var token = request.body.token || request.query.token || request.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, app.get('tokenKey'), function(error, decoded) {      
+      if (error) {
+        return response.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        request.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+    return response.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+  }
 });
 
 app.post('/kitten', function(request, response) {
