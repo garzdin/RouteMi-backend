751898976dfa5557dd02598a14594e2622f82a41
Teodor Garzdin <tsg@hugelawn.com>
2016-04-05 12:55:39 +0200

    Pass error to callback in password enctryption module.

--- utils/passwordEncryption.js
+++ utils/passwordEncryption.js
@@ -1,13 +1,19 @@
 var bcrypt = require('bcrypt-nodejs');
 
 exports.cryptPassword = function(password, callback) {
-    bcrypt.hash(password, 10, null, function(error, hash) {
-      return callback(error, hash);
-    });
+  bcrypt.hash(password, 10, null, function(error, hash) {
+    if(error) {
+      return callback(error, null);
+    }
+    return callback(null, hash);
+  });
 };
 
 exports.comparePassword = function(password, userPassword, callback) {
-   bcrypt.compare(password, userPassword, function(error, isPasswordMatch) {
-      return callback(error, isPasswordMatch);
-   });
+  bcrypt.compare(password, userPassword, function(error, isPasswordMatch) {
+    if(error) {
+      return callback(error, null);
+    }
+    return callback(null, isPasswordMatch);
+  });
 };
