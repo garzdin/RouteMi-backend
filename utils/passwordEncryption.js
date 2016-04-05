3268cb94adff836bf11defe6b0459f9cec7bc620
Teodor Garzdin <tsg@hugelawn.com>
2016-04-05 11:51:02 +0200

    More password hashing fixes.

--- utils/passwordEncryption.js
+++ utils/passwordEncryption.js
@@ -3,16 +3,16 @@ var bcrypt = require('bcrypt-nodejs');
 exports.cryptPassword = function(password, callback) {
     bcrypt.hash(password, 10, null, function(error, hash) {
       if(error) {
-        return callback(error, null);
+        return callback(error);
       }
-      return callback(null, hash);
+      return callback(error, hash);
     });
 };
 
 exports.comparePassword = function(password, userPassword, callback) {
    bcrypt.compare(password, userPassword, function(error, isPasswordMatch) {
       if(error) {
-        return callback(error, null);
+        return callback(error);
       }
       return callback(null, isPasswordMatch);
    });
