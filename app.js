1659ea915a564d74ec952283bf317249230d0437
Teodor Garzdin <tsg@hugelawn.com>
2016-04-08 15:39:17 +0200

    Remove some unneeded code from the bootstrapper.

--- app.js
+++ app.js
@@ -11,21 +11,8 @@ var poi =  require('./routes/poi');
 
 mongoose.connect(process.env.MONGOLAB_URL || 'mongodb://apiuser:4pv-aeh-PbH-Btw@ds013300.mlab.com:13300/routemiapi');
 
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Successfully connected to MongoLab.');
-});
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-  return response.send({ response: "Welcome to the API." })
-});
 
 app.post('/account/create', account.create);
 app.post('/authenticate', authentication);
@@ -43,6 +30,4 @@ app.post('/route/:route_id/poi/create', poi.create);
 app.put('/route/:route_id/poi/update/:id', poi.update);
 app.delete('/route/:route_id/poi/delete/:id', poi.delete);
 
-app.listen(app.get('port'), function() {
-  console.log('RouteMiAPI app is running on port', app.get('port'));
-});
+app.listen(process.env.PORT || 5000);
